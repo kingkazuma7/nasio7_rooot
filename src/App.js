340390d0fc3c368af5ddad29e863a5a5c1837f07
kingkazuma7 <your.nasionasio7@gmail.com>
2023-03-20 11:11:44 +0900

    react 全画面レイアウト

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <a
+          className="App-link"
+          href="https://tech.nasio7.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Blog
+        </a>
+        <a
+          className="App-link"
+          href="https://twitter.com/nasio777"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          twitter
+        </a>
         <a
           className="App-link"
           href="https://github.com/kingkazuma7"
