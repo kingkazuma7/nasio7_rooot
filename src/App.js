c5624b5046e569a145e89f45cd9cf242081cd913
kingkazuma7 <your.nasionasio7@gmail.com>
2023-03-25 16:50:52 +0900

    Router・Route、Linkに変更

--- src/App.js
+++ src/App.js
@@ -1,43 +1,34 @@
 import './App.scss';
 import { IconContext } from 'react-icons';
 import { FaGithub, FaTwitter, FaBlogger } from "react-icons/fa";
+import { Link, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <IconContext.Provider value={{ color: '#F2F2F2', size: ' 20px' }}>
-          <div className="App-header__inner">
-            <a
-              className="App-link"
-              href="https://tech.nasio7.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaBlogger />
-              Blog
-            </a>
-            <a
-              className="App-link"
-              href="https://twitter.com/nasio777"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaTwitter />
-              twitter
-            </a>
-            <a
-              className="App-link"
-              href="https://github.com/kingkazuma7"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaGithub />
-              GitHub
-            </a>
-          </div>
-        </IconContext.Provider>
-      </header>
+      <BrowserRouter>
+        <header className="App-header">
+          <IconContext.Provider value={{ color: '#F2F2F2', size: ' 20px' }}>
+            <div className="App-header__inner">
+              <Link 
+                className="App-link"
+                to="https://tech.nasio7.com/"
+                target="_blank"
+                rel="noopener noreferrer"><FaBlogger />Blog</Link>
+              <Link 
+                className="App-link"
+                to="https://twitter.com/nasio777"
+                target="_blank"
+                rel="noopener noreferrer"><FaTwitter />Twitter</Link>
+              <Link 
+                className="App-link"
+                to="https://github.com/kingkazuma7"
+                target="_blank"
+                rel="noopener noreferrer"><FaGithub />GitHub</Link>
+            </div>
+          </IconContext.Provider>
+        </header>
+      </BrowserRouter>
     </div>
   );
 }
