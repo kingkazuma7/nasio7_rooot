6552fb561f8d46768fb2afc34416de23785adb7d
kingkazuma7 <your.nasionasio7@gmail.com>
2023-03-29 18:52:45 +0900

    fix BrowserRouter delete

--- src/App.js
+++ src/App.js
@@ -8,34 +8,32 @@ function App() {
   return (
     <div className="container">
       <Profile />
-      <BrowserRouter>
-        <header className="App-header">
-          <IconContext.Provider value={{ color: '#F2F2F2', size: ' 20px' }}>
-            <div className="App-header__inner">
-              <a 
-                className="App-link"
-                href="https://tech.nasio7.com/"
-                target="_blank"
-                rel="noopener noreferrer"><FaBlogger />Blog</a>
-              <a 
-                className="App-link"
-                href="https://twitter.com/nasio777"
-                target="_blank"
-                rel="noopener noreferrer"><FaTwitter />Twitter</a>
-              <a 
-                className="App-link"
-                href="https://github.com/kingkazuma7"
-                target="_blank"
-                rel="noopener noreferrer"><FaGithub />GitHub</a>
-              <a 
-                className="App-link"
-                href="https://bookmeter.com/users/1402463"
-                target="_blank"
-                rel="noopener noreferrer"><FaBook />積読</a>
-            </div>
-          </IconContext.Provider>
-        </header>
-      </BrowserRouter>
+      <header className="App-header">
+        <IconContext.Provider value={{ color: '#F2F2F2', size: ' 20px' }}>
+          <div className="App-header__inner">
+            <a 
+              className="App-link"
+              href="https://tech.nasio7.com/"
+              target="_blank"
+              rel="noopener noreferrer"><FaBlogger />Blog</a>
+            <a 
+              className="App-link"
+              href="https://twitter.com/nasio777"
+              target="_blank"
+              rel="noopener noreferrer"><FaTwitter />Twitter</a>
+            <a 
+              className="App-link"
+              href="https://github.com/kingkazuma7"
+              target="_blank"
+              rel="noopener noreferrer"><FaGithub />GitHub</a>
+            <a 
+              className="App-link"
+              href="https://bookmeter.com/users/1402463"
+              target="_blank"
+              rel="noopener noreferrer"><FaBook />積読</a>
+          </div>
+        </IconContext.Provider>
+      </header>
     </div>
   );
 }
